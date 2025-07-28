cdde8cc8e0201461e9e6b22fe44e04b80ed6e81e
dazavv <dasha7348@gmail.com>
2025-07-28 21:25:31 +0300

    rest api done

--- src/main/java/org/example/prototype/web/controller/CommandController.java
+++ src/main/java/org/example/prototype/web/controller/CommandController.java
@@ -1,17 +1,22 @@
 package org.example.prototype.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.example.prototype.web.model.AuthorType;
 import org.example.prototype.web.model.CommandType;
 import org.example.prototype.web.service.CommandService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "command")
+@RequiredArgsConstructor
 public class CommandController {
-    CommandService commandService;
+    private final CommandService commandService;
 
     @PostMapping
-    public void runCommand(AuthorType authorType, CommandType commandType) {
+    public void runCommand(@RequestParam AuthorType authorType, @RequestParam CommandType commandType) {
         commandService.runCommand(authorType, commandType);
     }
 }
--- src/main/java/org/example/prototype/web/service/CommandService.java
+++ src/main/java/org/example/prototype/web/service/CommandService.java
@@ -1,13 +1,35 @@
 package org.example.prototype.web.service;
 
+import lombok.RequiredArgsConstructor;
 import org.example.prototype.web.model.AuthorType;
 import org.example.prototype.web.model.CommandType;
+import org.example.starter.audit.annotation.WeylandWatchingYou;
 import org.example.starter.command.model.Command;
+import org.example.starter.command.model.CommandPriority;
+import org.example.starter.command.service.executors.CommandQueueService;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
+@Service
+@RequiredArgsConstructor
 public class CommandService {
+    private final CommandQueueService commandQueueService;
+
+    @WeylandWatchingYou
     public void runCommand(AuthorType authorType, CommandType commandType) {
         Command newCommand = new Command();
 
+        newCommand.setDescription(commandType.getDescription());
+        newCommand.setPriority(choosePriority(authorType));
+        newCommand.setAuthor(authorType.getDescription());
+        newCommand.setTime(String.valueOf(LocalDateTime.now()));
 
+        commandQueueService.processCommand(newCommand);
+    }
+    private CommandPriority choosePriority(AuthorType author) {
+        return switch (author) {
+            case LIEUTENANT_ELLEN_RIPLEY, CAPTAIN_DALLAS -> CommandPriority.CRITICAL;
+            default -> CommandPriority.COMMON;
+        };
     }
 }
